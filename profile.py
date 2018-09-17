f64e5ac88e143d1af79ff349c1c01881e14ffb90
beccabuoni <38893323+beccabuoni@users.noreply.github.com>
2018-09-17 09:59:09 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -5,53 +5,36 @@ import geni.portal as portal
 import geni.rspec.pg as rspec
 import geni.rspec.igext
 
-# Describe the parameter(s) this profile script can accept.
+# Define parameters
 portal.context.defineParameter( "n", "Number of VMs", portal.ParameterType.INTEGER, 4 )
-#portal.context.defineParameter( "os", "disk image", portal.ParameterType.DISK_IMAGE, "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD")
 
-# Retrieve the values the user specifies during instantiation.
+# Get user specified values
 params = portal.context.bindParameters()
 
-# Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
+#Create portal context
+pc = portal.Context()
 
-# Check parameter validity.
+# Create a Request object to start building the RSpec
+request = pc.makeRequestRSpec()
+
+# Check parameters
 if params.n < 1 or params.n > 4:
-    portal.context.reportError( portal.ParameterError( "You must choose at least 1 and no more than 4 VMs." ) )
+    pc.reportError( portal.ParameterError( "You must choose at least 1 and no more than 4 VMs." ) )
 
 link = request.LAN("lan")
-    
+
+# Use a loop to create and assign values to XenVM nodes
 for i in range( params.n ):
-    # Create a XenVM and add it to the RSpec.
     node = request.XenVM( "node-" + str( i + 1) )
     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
     if (i + 1) == 1:
         node.routable_control_ip = True
-    iface = node.addInterface("if1")
+    inface = node.addInterface("if1")
     
-    # Specify the component id and the IPv4 address
-    iface.component_id = "eth1"
-    iface.addAddress(rspec.IPv4Address("192.168.1." + str( i + 1 ), "255.255.255.0"))
+    # Assign component id and IPv4 address
+    inface.component_id = "eth1"
+    inface.addAddress(rspec.IPv4Address("192.168.1." + str( i + 1 ), "255.255.255.0"))
 
-    link.addInterface(iface)
+    link.addInterface(inface)
 
-#node1 = request.XenVM("node1")
-#iface1 = node1.addInterface("if1")
-
-## Specify the component id and the IPv4 address
-#iface1.component_id = "eth1"
-#iface1.addAddress(rspec.IPv4Address("192.168.1.1", "255.255.255.0"))
-
-#node2 = request.XenVM("node2")
-#iface2 = node2.addInterface("if2")
-
-## Specify the component id and the IPv4 address
-#iface2.component_id = "eth2"
-#iface2.addAddress(rspec.IPv4Address("192.168.1.2", "255.255.255.0"))
-
-#link = request.LAN("lan")
-
-#link.addInterface(iface1)
-#link.addInterface(iface2)
-
-portal.context.printRequestRSpec()
+pc.printRequestRSpec()
